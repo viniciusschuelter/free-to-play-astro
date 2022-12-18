da0b2264b527b19b21d5727985b1ba2cdea954b0
VInicius Schuelter <48526428+viniciusschuelter@users.noreply.github.com>
2022-12-18 18:17:13 -0300

    feat(astro): fun with astro creating solid component

---
+++ src/components/solid-js/CardSolid.tsx
@@ -0,0 +1,22 @@
+import { render } from "solid-js/web";
+import type { GameInterface } from "../../interfaces/game.interface";
+
+const CardSolid = (props: { game: GameInterface }) => {
+    const game: GameInterface = props.game;
+    return (
+        <li className="link-card solid-js">
+            <a href={game.game_url}>
+                <img src={game.thumbnail} alt={game.title} width="100%" />
+                <h2>
+                    {game.title}
+                    <span>&rarr;</span>
+                </h2>
+                <p>
+                    {game.short_description}
+                </p>
+            </a>
+        </li>
+    );
+} 
+
+export default CardSolid;
